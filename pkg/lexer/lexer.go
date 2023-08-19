86c1c569f561e766933c6b6b5760ec18969986e8
Tim Smith <tsontario@gmail.com>
2023-08-19 00:29:44 -0400

    basic token and lex

---
+++ pkg/lexer/lexer.go
@@ -0,0 +1,62 @@
+package lexer
+
+import "go_interpreter/pkg/token"
+
+type Lexer struct {
+	input        string
+	position     int  // Current position in input
+	readPosition int  // Current reading position (e.g. next char to be read)
+	ch           byte // Current char being examined
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar() // Prepare for next NextToken call
+	return tok
+}
+
+func newToken(tok token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tok,
+		Literal: string(ch),
+	}
+}
